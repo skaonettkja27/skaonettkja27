c8e2938d4c74f9c5ec438b8eefa7806859b49520
skaonettkja27 <tkjpleret25@gmail.com>
2024-08-08 19:48:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.info("Starting Localhost")
+console.warn("WARNING")
+console.error("EROR")
+
+function DP1() {
+    alert("Pengajar: BU HANA. 6 jam pelajaran")
+}
+function BIND() {
+    alert("PENGAJAR: Dra.Widiyani")
+}
+function BING() {
+    alert("PENGAJAR: Mutiari Ruswita Nila")
+}
+function IPAS() {
+    alert("PENGAJAR: Sri Purwanti")
+}
+function PAB() {
+    alert("PENGAJAR: Nurul Huda")
+}
+function DP2() {
+    alert("PENGAJAR: BP SUDARSONO")
+}
+function BK() {
+    alert("PENGAJAR: BU DEVI")
+}
+function MTK() {
+    alert("PENGAJAR: BP AGUS HERI")
+}
+function SI() {
+    alert("PENGAJAR: BP RIYAN HARIYANA")
+}
+function PJOK() {
+    alert("PENGAJAR: BU Siti Khoirotul Fatimah")
+}
+function SB() {
+    alert("PENGAJAR: BU Rini Lestari")
+}
+function JAWA() {
+    alert("PENGAJAR: BU Fatikha Aulia")
+}
+function PPKN() {
+    alert("PENGAJAR: Lusia Krisna Epi")
+}
